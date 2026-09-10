78381f3bb8fae35fbef32a983bb8721d2466fe7e
safeer <safeerzaid4@gmail.com>
2026-09-10 17:46:16 +0530

    Implement signup validation, password hashing and login authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
-
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
 
 export const signup = async (req,res) => {
@@ -22,10 +23,12 @@ export const signup = async (req,res) => {
       })
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     const newUser = await User.create({
       name,
       email,
-      password
+      password : hashedPassword,
     })
 
     res.status(201).json({
@@ -40,4 +43,46 @@ export const signup = async (req,res) => {
     })
   }
 } 
+
+
+// login--------------------------------
+
+export const login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please fill all the fields",
+      });
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+      return res.status(404).json({
+        message: "user not found",
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+    return res.status(401).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+
+  }
+};
+
 
